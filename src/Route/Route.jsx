9c0b4f3d176bdae857ddf32996fd136c33bb5188
Jubayer9 <121636287+Jubayer9@users.noreply.github.com>
2023-06-07 21:52:12 +0600

    some Route add

--- src/Route/Route.jsx
+++ src/Route/Route.jsx
@@ -21,6 +21,7 @@ export const router = createBrowserRouter([
             {
                 path:'signup',
                 element:<SingUp></SingUp>
+                
             }
         ]
     },
